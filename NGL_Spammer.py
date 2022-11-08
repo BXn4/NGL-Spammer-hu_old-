73eed994df3c7c1e0e64c5e3aeb5a2ac495eb0f8
Bence <78733248+BXn4@users.noreply.github.com>
2022-11-08 16:09:13 +0100

    Update NGL_Spammer.py

--- NGL_Spammer.py
+++ NGL_Spammer.py
@@ -2,6 +2,8 @@ import time
 import requests
 import os
 from random import *
+from colorama import Fore, Back, Style
+from datetime import datetime
 
 #.txt fájl beolvasás, és a tartalmának eltárolása
 print("A txt fájl beolvasása, kérlek várj!")
@@ -16,14 +18,18 @@ try:
         
 except (FileNotFoundError):
     print("Nem található a fájl!")
+
+    
 request = requests.Session()
 ngl_nev = input("NGL név: ")
-i = 1
-
+i = 0
+datum = datetime.now()
+ido = datum.strftime("%H:%M:%S")
+print(Fore.YELLOW + "[{}]".format(ido)+ Fore.WHITE + " >> Kezdés\n")
 while(True):
-    if(i < 16):
-        time.sleep(1)
-        kerdes = ''.join(choice(kerdesek))
+    if(i < 15):
+        time.sleep(0)
+        kerdes = (choice(kerdesek))
         url = f"https://ngl.link/{ngl_nev}"
         fej = {
             "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/102.0.0.0 Safari/537.36"
@@ -31,10 +37,12 @@ while(True):
         adat = {
             "question": kerdes
             }
-        print("Kérdés: {}".format(kerdes))
+        print(Fore.BLUE + "[Kérdés] " + Fore.WHITE + "{}".format(kerdes))
         elkuld = request.post(url, headers=fej, data=adat)
         i = i+1
-    if(i == 16):
-        print("A kérdések elküldve! 2 perc múlva ismétlem!")
+    if(i == 15):
+        datum = datetime.now()
+        ido = datum.strftime("%H:%M:%S")
+        print(Fore.GREEN + "[{}]".format(ido) + Fore.WHITE +" >> Szünet (2 perc)\n")
         time.sleep(120)
-        i = 1
+        i = 0
