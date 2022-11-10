f470ac0d5c5d6822f35ca6ee99bf40a423b98883
Bence <78733248+BXn4@users.noreply.github.com>
2022-11-10 22:31:54 +0100

    Elkezdtem csinálni a GUI verzióját is

---
+++ NGL_Spammer_GUI.py
@@ -0,0 +1,24 @@
+from tkinter import *
+import requests
+
+def nevellenorzes():
+    nglNev = nevInput.get("1.0",END)
+    nevHossz = len(nglNev)
+    if(nevHossz == 1):
+        print("Üres!")
+    else:
+        url = 'https://ngl.link/asd)'
+        valasz = (requests.get(url))
+        print(valasz)
+ablak = Tk()
+ablak.title("NGL Spammer GUI")
+ablak.configure(width=300, height=300)
+ablak.resizable(False, False)
+nevLbl = Label(ablak, text='NGL Név:')
+nevLbl.place(x=10,y=10)
+nevInput = Text(width=15, height=1)
+nevInput.place(x=70, y=10)
+nevGomb = Button(ablak, text="Ellenőrzés", command=nevellenorzes)
+nevGomb.place(x=200, y=10)
+
+ablak.mainloop()
