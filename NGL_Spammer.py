5189ecf945951381c56cbd13f594391de81452bd
Bence <78733248+BXn4@users.noreply.github.com>
2023-01-16 23:22:51 +0100

    Update NGL_Spammer.py

--- NGL_Spammer.py
+++ NGL_Spammer.py
@@ -4,6 +4,20 @@ import os
 from random import *
 from datetime import datetime
 
+global sorok
+sorokx = 0
+global cel
+cel = []
+global mennyiszer
+mennyiszer = 0
+with open("fiokok.txt", "r") as file:
+    for line in file:
+        # Do something with the line
+        cel.append(line)
+        sorokx += 1
+        #print(sorokx)
+
+
 #.txt fájl beolvasás, és a tartalmának eltárolása
 try:
     kerdesek = []
@@ -18,16 +32,17 @@ except (FileNotFoundError):
 
     
 request = requests.Session()
-ngl_nev = input("NGL név: ")
 i = 0
 datum = datetime.now()
 ido = datum.strftime("%H:%M:%S")
 print("\n[{}] >> Kezdés\n".format(ido))
+global jsor
+jsor = 0
 while(True):
     if(i < 15):
         time.sleep(1)
         kerdes = (choice(kerdesek))
-        url = f"https://ngl.link/{ngl_nev}"
+        url = f"https://ngl.link/{cel[jsor]}"
         fej = {
             "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/102.0.0.0 Safari/537.36"
         }
@@ -43,3 +58,8 @@ while(True):
         print("[{}] >> Szünet (2 perc)\n".format(ido))
         time.sleep(120)
         i = 0
+        jsor += 1
+        if jsor == sorokx:
+            jsor = 0
+            mennyiszer += 1
+            print(mennyiszer)
