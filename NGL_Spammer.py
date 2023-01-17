8961e543882ac19562a8aa57a9c21e201ee4393e
Bence <78733248+BXn4@users.noreply.github.com>
2023-01-17 20:34:36 +0100

    Több fiók kezelése

--- NGL_Spammer.py
+++ NGL_Spammer.py
@@ -1,63 +1,64 @@
 import time
 import requests
-import os
 from random import *
 from datetime import datetime
 
-global sorok
-sorokx = 0
-global cel
-cel = []
-global mennyiszer
-mennyiszer = 0
-with open("fiokok.txt", "r") as file:
-    for line in file:
-        # Do something with the line
-        cel.append(line)
-        sorokx += 1
+fiokoksora = 0
+jelenlegi = 0
+mennyitkuldott = []
+fiok = ""
 
-#.txt fájl beolvasás, és a tartalmának eltárolása
 try:
-    kerdesek = []
-    with open('kerdesek.txt', 'r', encoding="UTF-8") as kerdesolvas:
-        sorok = kerdesolvas.readline()
-        while sorok:
-            kerdesek.append(sorok)
-            sorok = kerdesolvas.readline()
-        
-except (FileNotFoundError):
-    print("Nem található a fájl!")
+  kerdesek = []
+  fiokok = []
+  with open("kerdesek.txt", "r", encoding="UTF-8") as kerdesolvas:
+    sorok = kerdesolvas.readline()
+    while sorok:
+      kerdesek.append(sorok)
+      sorok = kerdesolvas.readline()
+  with open("fiokok.txt", "r") as file:
+    for line in file:
+        fiokok.append(line)
+        mennyitkuldott.append(0)
+        fiokoksora += 1
 
-    
+except (FileNotFoundError):
+  print("Nem található a fájl!")
+
 request = requests.Session()
 i = 0
 datum = datetime.now()
 ido = datum.strftime("%H:%M:%S")
 print("\n[{}] >> Kezdés\n".format(ido))
-global jsor
-jsor = 0
-while(True):
-    if(i < 15):
-        time.sleep(1)
-        kerdes = (choice(kerdesek))
-        url = f"https://ngl.link/{cel[jsor]}"
-        fej = {
-            "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/102.0.0.0 Safari/537.36"
-        }
-        adat = {
-            "question": kerdes
-            }
-        print("[Kérdés] {}".format(kerdes))
-        elkuld = request.post(url, headers=fej, data=adat)
-        i = i+1
-    if(i == 15):
-        datum = datetime.now()
-        ido = datum.strftime("%H:%M:%S")
-        print("[{}] >> Szünet (2 perc)\n".format(ido))
-        time.sleep(120)
-        i = 0
-        jsor += 1
-        if jsor == sorokx:
-            jsor = 0
-            mennyiszer += 1
-            print(mennyiszer)
+
+while (True):
+  if (i < 10):
+    time.sleep(1)
+    fiok = fiokok[jelenlegi].strip()
+    kerdes = (choice(kerdesek))
+    url = f"https://ngl.link/{fiok}"
+
+    fejresz = {
+      "user-agent":
+      "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/102.0.0.0 Safari/537.36"
+    }
+
+    adat = {"question": kerdes}
+    mennyitkuldott[jelenlegi] += 1
+    print("-> %s (%s) \n[Kérdés] %s" % (fiok,mennyitkuldott[jelenlegi],kerdes))
+    elkuld = request.post(url, headers=fejresz, data=adat)
+    i = i + 1
+
+  if (i == 10):
+
+    print(fiokok[jelenlegi])
+    jelenlegi += 1
+
+    if jelenlegi == fiokoksora:
+      jelenlegi = 0
+
+    datum = datetime.now()
+    ido = datum.strftime("%H:%M:%S")
+    print("[{}] >> Szünet (2 perc)\n".format(ido))
+    time.sleep(120)
+    i = 0
