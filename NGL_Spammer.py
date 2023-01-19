f3380d88916548e3d9808e5944a35c3d5b2c300e
Bence <78733248+BXn4@users.noreply.github.com>
2023-01-20 00:01:04 +0100

    "Szebb kód"

--- NGL_Spammer.py
+++ NGL_Spammer.py
@@ -5,32 +5,27 @@ from datetime import datetime
 import string
 import uuid
 
-fiokoksora = 0
+fiokokszama = 0
 jelenlegi = 0
 mennyitkuldott = []
+kerdesek = []
+fiokok = []
 fiok = ""
 eszkozid = ""
 
 def eszkozidgeneralas():
     eszkozid = uuid.uuid4().hex
     return "-".join([eszkozid[i:i+8] for i in range(0, 32, 8)])
-
-eszkozid = eszkozidgeneralas()
 eszkozidgeneralas()
+eszkozid = eszkozidgeneralas()
 
 try:
-  kerdesek = []
-  fiokok = []
-  with open("kerdesek.txt", "r", encoding="UTF-8") as kerdesolvas:
-    sorok = kerdesolvas.readline()
-    while sorok:
-      kerdesek.append(sorok)
-      sorok = kerdesolvas.readline()
-  with open("fiokok.txt", "r") as file:
-    for line in file:
-        fiokok.append(line)
-        mennyitkuldott.append(0)
-        fiokoksora += 1
+    with open("kerdesek.txt", "r", encoding="UTF-8") as kerdesekolvas:
+        kerdesek = [sorok.strip() for sorok in kerdesekolvas]
+    with open("fiokok.txt", "r") as fiokokolvas:
+        fiokok = [sorok.strip() for sorok in fiokokolvas]
+    mennyitkuldott = [0 for _ in range(len(fiokok))]
+    fiokokszama = len(fiokok)
 
 except (FileNotFoundError):
   print("Nem található a fájl!")
@@ -41,8 +36,8 @@ datum = datetime.now()
 ido = datum.strftime("%H:%M:%S")
 print("\n[{}] >> Kezdés\n".format(ido))
 
-while (True):
-  if (i < 10):
+while True:
+  if (i < 2):
     time.sleep(1)
     fiok = fiokok[jelenlegi].strip()
     kerdes = (choice(kerdesek))
@@ -54,28 +49,24 @@ while (True):
       "user-agent":
       "Mozilla/5.0 (X11; Linux x86_64; rv:108.0) Gecko/20100101 Firefox/108.0"
     }
-
+    
     adat = {
     "username": fiok,
     "question": kerdes,
     "deviceId": eszkozid
     }
     mennyitkuldott[jelenlegi] += 1
-    print("-> %s (%s) \n[Kérdés] %s" % (fiok,mennyitkuldott[jelenlegi],kerdes))
+    print("-> %s (%s) \n[Kérdés] %s" % (fiok,mennyitkuldott[jelenlegi],kerdes) + "\n")
     elkuld = request.post("https://ngl.link/api/submit", headers=fejresz, data=adat)
     #print(elkuld) (Tesztelés)
     i = i + 1
 
-  if (i == 10):
-    
+  if (i == 2):
     eszkozidgeneralas()
-
-    print(fiokok[jelenlegi])
+    print("Következő: -> " + fiokok[jelenlegi + 1])
     jelenlegi += 1
-
-    if jelenlegi == fiokoksora:
+    if jelenlegi == fiokokszama:
       jelenlegi = 0
-
     datum = datetime.now()
     ido = datum.strftime("%H:%M:%S")
     print("[{}] >> Szünet (2 perc)\n".format(ido))
